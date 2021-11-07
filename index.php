ab6195e3e1712ce2dd87ffaf6c4d9a9eb74c8ede
Nics Projects <nicsprojects25@gmail.com>
2021-11-07 14:01:28 +0700

    it was born

---
+++ index.php
@@ -0,0 +1,76 @@
+<!DOCTYPE HTML>
+<html lang="en">
+    <head>
+        <title>My Inspiration</title>
+        <link href="css/default.css" rel="stylesheet">
+        <script src="js/jquery-2.1.4.js" type="text/javascript">
+        </script>
+        <script src="js/defautl.js" type="text/javascript"></script>
+    </head>
+    <body>
+        <header>
+            <nav>
+                <ul>
+                    <li>HOME</li>
+                    <li>PRODUCT</li>
+                    <li>FEEDBACK</li>
+                    <li>BLOG</li>
+                    <li>BUY</li>
+                    <li>PROFILE</li>
+                </ul>
+                <form action="" method="">
+                    <input type="text">
+                    <input type="button" value="cari">
+                </form>
+            </nav>
+            <div class="content-header">
+                <div class="isi-content-header">
+                    <p id="welcome">WELCOME TO FIND JOBS</p>
+                    <article>
+                        Anda sedang mencari pekerjaan ? atau sedang membutuhkan pekerja ? disini tempatnya, kamu dapat menjadi pekerja atau pun pencari pekerja yang tidak terikat kontrak dan bebas memilih.
+                    </article>
+                    <div class="example">
+                        <section>
+                            <p id="thumbnail">WEB DEVELOPER</p>
+                            <img src="img/logo-sponsor/essence-bakery-hamburger.jpg">
+                            <p>description of this job for example </p>
+                            <button>READ</button>
+                        </section>
+                        <section id="sec-2">                        <p id="thumbnail">WEB DEVELOPER</p>
+                            <img src="img/logo-sponsor/essence-bakery-hamburger.jpg">
+                            <p>description of this job for example </p>
+                            <button>READ</button>
+                        </section>
+                        <section id="sec-3">                        <p id="thumbnail">WEB DEVELOPER</p>
+                            <img src="img/logo-sponsor/essence-bakery-hamburger.jpg">
+                            <p>description of this job for example </p>
+                            <button>READ</button>
+                        </section>
+                    </div>
+                </div>
+            </div>
+        </header>
+        <div class="container">
+            <div class="sub-container" >
+                <section id="sc-1">
+                    <article>
+                        <p>TENTANG KAMI</p>
+                        Find job telah berdiri sejak tahun 1999 dan telah menghasilkan banyak freelancer yang betah untuk mencari pekerjaan sambilan atau pun yang tidak berkeinginan mencari pekerjaan tetap sebagai rutinitas Find job telah berdiri sejak tahun 1999 dan telah menghasilkan banyak freelancer yang betah untuk mencari pekerjaan sambilan atau pun yang tidak berkeinginan mencari pekerjaan tetap sebagai rutinitas Find job telah berdiri sejak tahun 1999 dan telah menghasilkan banyak freelancer yang betah untuk mencari pekerjaan sambilan atau pun yang tidak berkeinginan mencari pekerjaan tetap sebagai rutinitas</article>
+                </section>
+                <section id="sc-2">
+                    <div class="caption"></div>
+                </section>
+            </div>
+            <div class="sub-container">
+                <section>adadad</section>
+                <section>vvvvv</section>
+            </div>
+            <div class="sub-container">
+                <section>adadad</section>
+                <section>vvvvv</section>
+            </div>
+        </div>
+        <footer>
+        </footer>
+    </body>
+</html>
